4fbb4beee50778c59ed8de448e88646d86cd31b5
hershghaderi <75530628+hershghaderi@users.noreply.github.com>
2026-09-05 15:55:40 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,5 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.10.0")
     implementation("androidx.compose.material3:material3:1.3.1")
 
-    // Tesseract OCR
     implementation("cz.adaptech.tesseract4android:tesseract4android:4.9.0")
 }
